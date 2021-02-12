c738ab73d1a77e8952810982fdc7cb8f8bf2f21d
devika100110 <77834219+devika100110@users.noreply.github.com>
2021-02-12 16:05:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+var object,ground ;
+function setup(){
+  var canvas=createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+var ground_options={
+  isStatic : true 
+}
+
+ground = Bodies.rectangle(200,390,400,20,ground_options);
+World.add(world,ground);
+
+var object_options = {
+  restitution: 1.0
+}
+object =Bodies.rectangle(200,100,50,50,object_options);
+World.add(world,object);
+
+console.log(object);
+}
+
+function draw()
+{
+  background(0);
+  Engine.update(engine);
+  rectMode(CENTER);
+    rect(object.position.x,object.position.y,50,50);
+  rect(ground.position.x,ground.position.y,400,20); 
+}
